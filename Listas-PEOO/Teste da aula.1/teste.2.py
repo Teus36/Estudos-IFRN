f7c08e8e2bfd8ca8db7ada26a59cf450fe11e154
Teus36 <mateus_batistaalmeida@hotmail.com>
2023-09-05 14:22:43 -0300

    update

--- Listas-PEOO/Teste da aula.1/teste.2.py
+++ Listas-PEOO/Teste da aula.1/teste.2.py
@@ -1,1 +1,23 @@
 from tkinter import *
+
+class app:
+    def __init__(self):
+        self.janela = Tk()
+        self.janela.title("Janela legal")
+        self.janela.geometry("1200x800")
+
+
+        self.frame1 = Frame(self.janela)
+        self.frame1.pack(side='right')
+
+        self.texto1 = Frame(
+            self.frame1,
+            text='Nome'
+        )
+        self.texto1.pack(side='left')
+        
+        self.janela.mainloop()
+
+
+aplicar = app()
+    
