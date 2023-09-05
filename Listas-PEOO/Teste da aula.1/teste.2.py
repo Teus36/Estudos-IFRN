85447b811f1678493246b6903abcaaf2e1952399
Teus36 <mateus_batistaalmeida@hotmail.com>
2023-09-05 14:30:48 -0300

    update

--- Listas-PEOO/Teste da aula.1/teste.2.py
+++ Listas-PEOO/Teste da aula.1/teste.2.py
@@ -8,14 +8,26 @@ class app:
 
 
         self.frame1 = Frame(self.janela)
-        self.frame1.pack(side='right')
+        self.frame1.pack()
 
-        self.texto1 = Frame(
+        self.texto1 = Label(
             self.frame1,
             text='Nome'
         )
         self.texto1.pack(side='left')
-        
+
+        self.texto2 = Label(
+            self.frame1,
+            text='Senha'
+        )
+        self.texto2.pack()
+
+        self.caixa1= Entry(self.frame1)
+        self.caixa1.pack()
+
+        self.caixa2= Entry(self.frame1)
+        self.caixa2.pack()
+
         self.janela.mainloop()
 
 
