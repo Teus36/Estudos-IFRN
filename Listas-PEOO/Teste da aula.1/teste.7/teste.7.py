2981c195d781af22054f7a777b882a3d5af8c1c5
Teus36 <mateus_batistaalmeida@hotmail.com>
2023-09-13 21:28:42 -0300

    update

---
+++ Listas-PEOO/Teste da aula.1/teste.7/teste.7.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+class app:
+    def __init__(self):
+        self.janela = Tk()
+        self.janela.geometry("860x620")
+        self.janela.title("Teste.7")
+
+        self.janela.mainloop()
+
+
+aplicar = app()
