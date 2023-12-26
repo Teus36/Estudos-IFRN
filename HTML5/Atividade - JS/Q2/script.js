492085cab4baa58a1078993ef347f0f680b9bf16
Mateus Batista <mateus_batistaalmeida@hotmail.com>
2023-12-26 12:56:06 -0300

    update

---
+++ HTML5/Atividade - JS/Q2/script.js
@@ -0,0 +1,5 @@
+ocultar.addEventListener('click', function() {
+    const menu = document.querySelector('.menu')
+    menu.classList.toggle("menu_oculto");
+    ocultar.classList.toggle("menu");
+});
