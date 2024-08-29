ca28d8a957a80101bdbdcd3a882c8c5e5317ab07
Mateus Batista <mateus_batistaalmeida@hotmail.com>
2024-08-29 12:32:50 -0300

    updt

---
+++ PSI1V/Prova3_PSI/init_db.py
@@ -0,0 +1,12 @@
+import sqlite3
+
+conn = sqlite3.connect('database.db')
+
+SCHEMA = "database/schema.sql"
+
+with open(SCHEMA) as f:
+    conn.executescript(f.read())
+
+# encerra operações
+conn.commit()
+conn.close()
