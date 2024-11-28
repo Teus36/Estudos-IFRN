108c4a3bfc3bff603ac3cb727c7f0a45e7161024
Teus36 <mateus_batistaalmeida@hotmail.com>
2024-11-27 23:24:07 -0300

    upd

--- PSI1V/Prova-5-PSI/books/books.py
+++ PSI1V/Prova-5-PSI/books/books.py
@@ -1,14 +1,17 @@
 from flask import Flask, render_template, url_for, request, Blueprint, redirect
 from books.models import Book
 from users.models import User
+from flask_login import login_required
 
 bp = Blueprint('books', __name__, url_prefix='/books', template_folder='templates')
 
 @bp.route('/')
+@login_required
 def index():
     return render_template('books/index.html', books = Book.all())
 
 @bp.route('/register', methods=['POST', 'GET'])
+@login_required
 def register():
     if request.method == 'POST':
         titulo = request.form['titulo']
--- PSI1V/Prova-5-PSI/venv/Scripts/activate.bat
+++ PSI1V/Prova-5-PSI/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\20221101110064\Documents\GitHub\Estudos\Estudos\PSI1V\Prova-5-PSI\venv
+set VIRTUAL_ENV=C:\Users\Jailson\Documents\GitHub\Estudos\PSI1V\Prova-5-PSI\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
