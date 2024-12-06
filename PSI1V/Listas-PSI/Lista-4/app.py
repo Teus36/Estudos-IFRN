d9f3253bfc9c7ba006dd0c599e52a77b8ce61eca
Mateus Batista <mateus_batistaalmeida@hotmail.com>
2024-12-06 11:46:09 -0300

    upd

--- PSI1V/Listas-PSI/Lista-4/app.py
+++ PSI1V/Listas-PSI/Lista-4/app.py
@@ -2,3 +2,17 @@ from flask import Flask, request, render_template, session, redirect, url_for, m
 
 app = Flask(__name__)
 app.secret_key = 'chave-secreta' # Necessário para usar sessões
+
+@app.route('/sessao', methods=['GET', 'POST'])
+def iniciarSessao():
+    if request.method == 'POST':
+        session['nome'] = request.form['nome']
+        return redirect(url_for('iniciarSessao'))
+    nome = session.get('nome')
+    return render_template('sessao.html', nome=nome)
+
+@app.route('/limpar_sessao')
+def limparSessao():
+    session.pop('nome', None)
+    return redirect(url_for('limparSessao'))
+
